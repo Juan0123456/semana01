6c988f22bcc74e034060121caeeae5bb55c21610
unknown <SUITE@SUITESM701-ST09.cibertec.edu.pe>
2019-03-25 21:09:15 -0500

    first commit

---
+++ web/servicios-semana01/servicios-semana01/Controllers/NegociosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace servicios_semana01.Controllers
+{
+    public class NegociosController : Controller
+    {
+        // GET: Negocios
+        public ActionResult Index()//pagina de inicio
+        {
+            return View();
+        }
+    }
+}
---
+++ web/servicios-semana01/servicios-semana01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="servicios_semana01.MvcApplication" Language="C#" %>
